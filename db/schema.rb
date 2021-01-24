8e5b04e4237022aa8f3e5af6dc37340ed3d51d9e
江原巧 <e.t.miiii228@gmail.com>
2021-01-24 23:28:59 +0900

    1対多の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_031534) do
+ActiveRecord::Schema.define(version: 2021_01_24_135550) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_01_23_031534) do
     t.string "email"
   end
 
+  add_foreign_key "articles", "users"
 end

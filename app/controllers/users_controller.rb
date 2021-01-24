d8483fbdf66503eb0ff2cad32c26940e00126344
江原巧 <e.t.miiii228@gmail.com>
2021-01-24 22:35:03 +0900

    CRUD処理の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,50 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+
   end
+
+  def create
+    # #インスタンスをmodelから作成
+    # @user = User.new(
+    #   name: params[:name],
+    #   account: params[:account],
+    #   email: params[:email],
+    # )
+    @user = User.new(user_params)
+
+    #インスタンスをDBに保存する
+    @user.save!
+
+    render :show
+  end
+
+  def update
+    #対象のレコードを探す
+
+    #探してきたレコードに対して変更を行う
+    @user.update!(user_params)
+    #jsonとして値を返す
+    render :show
+  end
+
+  def destroy
+
+    @user.destroy!
+    render :show
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :account, :email)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
